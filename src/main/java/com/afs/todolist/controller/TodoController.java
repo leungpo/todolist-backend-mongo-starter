ef959d1c0b0e8f59b996ba8c5637cbebd52fbcbf
leungpo <polly.leung@oocl.com>
2022-12-15 13:33:26 +0800

    feat: add get API for mongo

--- src/main/java/com/afs/todolist/controller/TodoController.java
+++ src/main/java/com/afs/todolist/controller/TodoController.java
@@ -3,9 +3,7 @@ package com.afs.todolist.controller;
 import com.afs.todolist.controller.mapper.TodoMapper;
 import com.afs.todolist.entity.Todo;
 import com.afs.todolist.service.TodoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
